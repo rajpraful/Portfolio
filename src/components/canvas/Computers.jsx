f06ecb125fb61da2f3db042680ccbe9201868d21
rajpraful <prafulrajhm@gmail.com>
2023-10-14 23:14:17 +0530

    fix - mobile loader

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -60,6 +60,7 @@ const ComputersCanvas = () => {
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
+      className="canvas-container"
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
